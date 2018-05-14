19fb9b2ffa598073dc9bfe8f10c454bfbfb12b33
Sibakov Kirill (Desctop) <kirill2001.91@mail.ru>
2018-05-14 23:00:44 +0300

    Добавьте файлы проекта.

---
+++ SnowBoardMarket/src/boot-client.tsx
@@ -0,0 +1,48 @@
+﻿import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import { createStore, applyMiddleware } from 'redux';
+import { Route } from 'react-router';
+import createHistory from 'history/createBrowserHistory';
+import { routerMiddleware, ConnectedRouter, routerReducer } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import { reducers, ApplicationState } from 'reducers';
+import App from 'containers/App'
+
+const history = createHistory();
+const middleware = routerMiddleware(history);
+
+const store = createStore(reducers, composeWithDevTools(
+    applyMiddleware(
+        thunk,
+        middleware
+    )
+));
+
+const render = App => {
+    ReactDOM.render(
+        <Provider store={store}>
+            <HashRouter>
+                <AppContainer>
+                    <App />
+                </AppContainer>
+            </HashRouter>
+        </Provider>,
+        document.getElementById('root'),
+    )
+}
+
+render(App)
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+    module.hot.accept('containers/App', () => {
+        // if you are using harmony modules ({modules:false})
+        render(App)
+        // in all other cases - re-require App manually
+        render(require('containers/App'))
+    })
+}
---
+++ SnowBoardMarket/webpack.config.js
@@ -0,0 +1,69 @@
+﻿const path = require('path');
+const webpack = require('webpack');
+
+module.exports = (env) => {
+    const isDevBuild = !(env && env.prod);
+    const clientBuldleOutputDir = './wwwroot/dist';
+
+    const clientBundleConfig = {
+        resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+        entry: {
+            'main': './src/boot-client.tsx'
+        },
+        output: {
+            filename: '[name].js',
+            path: path.join(__dirname, clientBuldleOutputDir),
+            publicPath: 'dist/'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                            options: {
+                                babelrc: true,
+                                plugins: ['react-hot-loader/babel'],
+                            },
+                        },
+                        {
+                            loader: 'awesome-typescript-loader?silent=true',
+                        }
+                    ]
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        {
+                            loader: 'style-loader'
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                camelCase: true,
+                                modules: true
+                            }
+                        },
+                        {
+                            loader: 'typescript-css-modules-loader'
+                        }
+                    ]
+                },
+                {
+                    test: /\.json$/,
+                    use: [
+                        {
+                            loader: 'json-loader'
+                        }
+                    ]
+                }
+            ]
+        },
+        mode: isDevBuild ? 'development' : 'production',
+        plugins: [
+        ]
+    };
+
+    return [clientBundleConfig];
+}
