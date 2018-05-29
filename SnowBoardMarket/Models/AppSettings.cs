12c8990983c312b71931858644f5b9077010fc8a
Sibakov Kirill (Desctop) <kirill2001.91@mail.ru>
2018-05-29 23:03:00 +0300

    try

--- SnowBoardMarket/Models/AppSettings.cs
+++ SnowBoardMarket/Models/AppSettings.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SnowBoardMarket.Extentions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,15 @@ namespace SnowBoardMarket.Models
     public class AppSettings
     {
         public bool EnableSSl { get; set; }
+
         public string Domain { get; set; }
+        public string DomainUri
+        {
+            get
+            {
+                return this.GetAppUri();
+            }
+        }
+        public string BasketUri { get; set; }
     }
 }
